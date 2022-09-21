7d17d758417db58de69ce67c27ae22fb722f568a
ihaha424 <70957529+ihaha424@users.noreply.github.com>
2022-09-21 19:11:41 +0900

    CutSin 직접 설정해서 넣을 수 있게 만들고 그게 맞는 창 만들기

--- ShortTrack/frame_divide.py
+++ ShortTrack/frame_divide.py
@@ -2,7 +2,7 @@ import cv2 #OpenCV 라이브러리
 import os   #파일 디렉토리 라이브러리
 import shutil
 def frame_divide(video):
-    imagePath = './ShortTrack/images/'
+    imagePath = './images/'
     try:
         #임시로 파일이 있으면 삭제하는거 만든거
         if os.path.exists(imagePath):
--- ShortTrack/frame_merge.py
+++ ShortTrack/frame_merge.py
@@ -1,18 +1,20 @@
 from turtle import width
 from moviepy.editor import *
 
-def frame_merge(video,total_score):
+def frame_merge(video,total_score,start,sin,finish):
     #임시로 만들어 본거
     total_score[5]=5
     total_score[20]=6
     total_score[10]=8
     print(total_score)
+    #여기까지
+    
     #하이라이트 갯수
     max_highlight=3
     highlight = []
-    Start = VideoFileClip("./ShortTrack/CutSin/Start.mp4").subclip(0,1)
-    Sin=VideoFileClip("./ShortTrack/CutSin/Sin.mp4")
-    Finish=VideoFileClip("./ShortTrack/CutSin/Finish.mp4").subclip(0,1)
+    Start = VideoFileClip(start).subclip(0,1)
+    Sin=VideoFileClip(sin)
+    Finish=VideoFileClip(finish).subclip(0,1)
     #스코어 가장 큰거 찾기
     for j in range(0,max_highlight):        
         for i in range(0,len(total_score)):
@@ -26,4 +28,4 @@ def frame_merge(video,total_score):
             total_score[score_frame+k]=0
 
     output = concatenate_videoclips([Start,highlight[0],Sin,highlight[1],Sin,highlight[2],Finish],method="compose")        
-    output.write_videofile("./ShortTrack/outputVideo/output.mp4")
+    output.write_videofile("./outputVideo/output.mp4")
