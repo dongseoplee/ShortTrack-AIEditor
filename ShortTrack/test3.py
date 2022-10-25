f6aa6239569cfc7fada933e3d5e7807a008f7aa8
kokocom0113 <105492261+mingue98@users.noreply.github.com>
2022-10-25 20:26:20 +0900

    upload stt

--- ShortTrack/test3.py
+++ ShortTrack/test3.py
@@ -8,28 +8,27 @@ import numpy as np
 from matplotlib import pyplot as plt
 import moviepy.editor as mp
 
-clip = mp.VideoFileClip("short_cut2.mp4")  
-clip.audio.write_audiofile("audio.wav")
+clip = mp.VideoFileClip("short_cut2.mp4")  #처음 wav파일로 바꿀 mp4파일 것
+clip.audio.write_audiofile("audio.wav")    #저장할 wav파일 이름 설정
 
-sample_rate, data = wavfile.read('audio.wav')
+sample_rate, data = wavfile.read('audio.wav') #저장한 wav파일을 읽음
 arr1, arr2 = np.split(data, 2, axis=1)
 
 time = np.linspace(0, len(data) / sample_rate, len(data))
 
-plt.figure(figsize=(20, 10))
 
 
 times = np.arange(len(data)) / sample_rate
 up_frequency = times[np.argwhere(np.squeeze(arr2) >= 28000)].squeeze() #주파수 28000이상 저장
 
-for i in range(len(up_frequency)):         #int형으로 변환
+for i in range(len(up_frequency)):  #int형으로 변환
     up_frequency[i] = int(up_frequency[i])
 
     
 up_frequency = list(set(up_frequency))
 
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\박민규\Downloads\short-test-359905-6d7af470e878.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"short-test-359905-6d7af470e878.json"
 
 client = speech.SpeechClient()
 
@@ -39,7 +38,7 @@ buckets = list(storage_client.list_buckets())
 
 
 bucket_name = 'short_test'  # 서비스 계정 생성한 bucket 이름 입력
-source_file_name = r'C:\Users\박민규\Desktop\python\audio.wav'  # GCP에 업로드할 파일 절대경로
+source_file_name = r'audio.wav'  # GCP에 업로드할 파일 경로
 destination_blob_name = 'audio.wav'  # 업로드할 파일을 GCP에 저장할 때의 이름
 
 storage_client = storage.Client()
@@ -50,6 +49,9 @@ blob.upload_from_filename(source_file_name)
 
 
 word_time = [] #키워드 시간초 저장
+
+
+keyword = ["일본", "중국"]
 
 def transcribe_gcs(gcs_uri):
     client = speech.SpeechClient()
@@ -66,23 +68,25 @@ def transcribe_gcs(gcs_uri):
 
 
     for result in response.results:
-        sentence = []
         alternative = result.alternatives[0]
-        sentence.append(result.alternatives[0].transcript)
         for word_info in alternative.words:
             word = word_info.word
             start_time = word_info.start_time.total_seconds()
            
-            val = word_info.word.find("일본")
-            
-            if val != -1:
+
+            if word_info.word in keyword:
                 word_time.append(start_time)
+                
 
 
     return response
 
 
-response = transcribe_gcs("gs://short_test/audio.wav")
+
+response = transcribe_gcs("gs://short_test/audio.wav") # 구글 안에 있는 STT 파일이라서 바꾸면 안될듯
+
+
+
 
 
 print("단어나오는 시간")
@@ -91,3 +95,73 @@ print(word_time)
 print("주파수 28000이상")
 print(up_frequency)
 print("completed")
+
+def STT_detection(video,count=35):
+
+    keyword = ["일본", "중국"]# 추출할 키워드들 
+
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"short-test-359905-6d7af470e878.json"
+
+    client = speech.SpeechClient()
+
+    storage_client = storage.Client()
+    buckets = list(storage_client.list_buckets())
+
+    clip = mp.VideoFileClip("short_cut2.mp4")  
+    clip.audio.write_audiofile("audio.wav")
+
+    bucket_name = 'short_test'  # 서비스 계정 생성한 bucket 이름 입력
+    source_file_name = r'audio.wav'  # GCP에 업로드할 파일 절대경로
+    destination_blob_name = 'audio.wav'  # 업로드할 파일을 GCP에 저장할 때의 이름
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name) 
+
+    blob.upload_from_filename(source_file_name)
+
+
+
+
+    
+
+    def transcribe_gcs(gcs_uri):
+
+        word_time=[]
+        client = speech.SpeechClient()
+        audio = speech.RecognitionAudio(uri=gcs_uri)
+        config = speech.RecognitionConfig(
+            encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=44100,
+            language_code='ko-KR',
+            enable_word_time_offsets=True,
+            audio_channel_count=2)
+        operation = client.long_running_recognize(request={"config": config, "audio": audio})
+        response = operation.result()
+
+
+
+        for result in response.results:
+            alternative = result.alternatives[0]
+            for word_info in alternative.words:
+                word = word_info.word
+                start_time = word_info.start_time.total_seconds()
+            
+
+                if word_info.word in keyword:
+                    word_time.append(start_time)
+                    
+
+
+        return word_time
+
+
+
+    word_time = transcribe_gcs("gs://short_test/audio.wav")
+
+    score=[0 for i in range(count)]
+    for i in word_time:
+        frame_index = i/1
+        score[frame_index] = 1 
+        
+    return score
