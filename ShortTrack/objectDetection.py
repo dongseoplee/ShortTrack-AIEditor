cdc3435797d6da026feb1734b8cfe262995c8fda
Kim Y.C <yckim19@naver.com>
2022-10-25 18:51:50 +0900

    update objectDetection

--- ShortTrack/objectDetection.py
+++ ShortTrack/objectDetection.py
@@ -6,21 +6,21 @@ import os
 target=0 #fall_down의 index
 threshhold=0.7 #객체인식 임계 확률
 
-model = torch.hub.load("C:/Users/KIM YAECHAN/Desktop/졸업작품/content/yolov5", 'custom', path="C:/Users/KIM YAECHAN/Desktop/졸업작품/content/yolov5/runs/train/skate3/weights/best.pt", source='local')
+# model = torch.hub.load("C:/Users/KIM YAECHAN/Desktop/졸업작품/content/yolov5", 'custom', path="C:/Users/KIM YAECHAN/Desktop/졸업작품/content/yolov5/runs/train/skate3/weights/best.pt", source='local')
 
-img = 'C:/Users/KIM YAECHAN/Desktop/졸업작품/val/AnyConv.com__8.jpg'
-test='C:/Users/KIM YAECHAN/Documents/GitHub/ShortTrack-AIEditor/ShortTrack/images/temp/frame0.png'
-video='C:/Users/KIM YAECHAN/Desktop/졸업작품/test_video.mp4'
+# img = 'C:/Users/KIM YAECHAN/Desktop/졸업작품/val/AnyConv.com__8.jpg'
+# test='C:/Users/KIM YAECHAN/Documents/GitHub/ShortTrack-AIEditor/ShortTrack/images/temp/frame0.png'
+# video='C:/Users/KIM YAECHAN/Desktop/졸업작품/test_video.mp4'
 
 
 
-def Object_detection(images,target,threshhold):
+def Object_detection(target='fall_down',threshhold=0.5):
     
     target_id_to_name = ['fall_down','skating','start', 'finish']
     target_name_to_id = {target_id_to_name[i]: i for i in range(len(target_id_to_name))}
     
     #모델 파라미터 load
-    model = torch.hub.load("C:/Users/KIM YAECHAN/Desktop/졸업작품/content/yolov5", 'custom', path="C:/Users/KIM YAECHAN/Desktop/졸업작품/content/yolov5/runs/train/skate3/weights/best.pt", source='local')
+    model = torch.hub.load("yolov5", 'custom', path="../best.pt", source='local')
     
     #객체를 index번호로 바꿈
     target=target_name_to_id[target]
@@ -44,11 +44,9 @@ def Object_detection(images,target,threshhold):
                     count_object+=1
 
         score.append(count_object)
-        # results.show()  
+        results.show()  
         
     return score
 
-
-target_id_to_name = ['fall_down','skating']
-target_name_to_id = {target_id_to_name[i]: i for i in range(len(target_id_to_name))}
-print(target_name_to_id['fall_down'])
+result=Object_detection()
+print(result)
