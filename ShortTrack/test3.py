253e3e29c8c584704404780666578537b101927e
kokocom0113 <105492261+mingue98@users.noreply.github.com>
2022-11-22 16:04:10 +0900

    json update

--- ShortTrack/test3.py
+++ ShortTrack/test3.py
@@ -28,7 +28,7 @@ for i in range(len(up_frequency)):  #int형으로 변환
 up_frequency = list(set(up_frequency))
 
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"short-test-359905-6d7af470e878.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"pure-anthem-369402-9bcdbdacbb25.json"
 
 client = speech.SpeechClient()
 
@@ -37,7 +37,7 @@ buckets = list(storage_client.list_buckets())
 
 
 
-bucket_name = 'short_test'  # 서비스 계정 생성한 bucket 이름 입력
+bucket_name = 'short_track-ai_editor'  # 서비스 계정 생성한 bucket 이름 입력
 source_file_name = r'audio.wav'  # GCP에 업로드할 파일 경로
 destination_blob_name = 'audio.wav'  # 업로드할 파일을 GCP에 저장할 때의 이름
 
@@ -78,13 +78,14 @@ def transcribe_gcs(gcs_uri):
                 word_time.append(start_time)
                 
 
-
+    
+    
+ 
     return response
 
 
 
-response = transcribe_gcs("gs://short_test/audio.wav") # 구글 안에 있는 STT 파일이라서 바꾸면 안될듯
-
+response = transcribe_gcs("gs://short_track-ai_editor/audio.wav") # 구글 안에 있는 STT 파일이라서 바꾸면 안될듯
 
 
 
@@ -96,72 +97,24 @@ print("주파수 28000이상")
 print(up_frequency)
 print("completed")
 
-def STT_detection(video,count=35):
 
-    keyword = ["일본", "중국"]# 추출할 키워드들 
+def Frequency_score(up_frequency, count = 35):
 
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"short-test-359905-6d7af470e878.json"
-
-    client = speech.SpeechClient()
-
-    storage_client = storage.Client()
-    buckets = list(storage_client.list_buckets())
-
-    clip = mp.VideoFileClip("short_cut2.mp4")  
-    clip.audio.write_audiofile("audio.wav")
-
-    bucket_name = 'short_test'  # 서비스 계정 생성한 bucket 이름 입력
-    source_file_name = r'audio.wav'  # GCP에 업로드할 파일 절대경로
-    destination_blob_name = 'audio.wav'  # 업로드할 파일을 GCP에 저장할 때의 이름
-
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name) 
-
-    blob.upload_from_filename(source_file_name)
-
-
-
-
+    score=[0 for i in range(count)]
+    for i in up_frequency:
+        frame_index = i/1
+        score[frame_index] = 1 
     
-
-    def transcribe_gcs(gcs_uri):
-
-        word_time=[]
-        client = speech.SpeechClient()
-        audio = speech.RecognitionAudio(uri=gcs_uri)
-        config = speech.RecognitionConfig(
-            encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-            sample_rate_hertz=44100,
-            language_code='ko-KR',
-            enable_word_time_offsets=True,
-            audio_channel_count=2)
-        operation = client.long_running_recognize(request={"config": config, "audio": audio})
-        response = operation.result()
+    return score
 
 
-
-        for result in response.results:
-            alternative = result.alternatives[0]
-            for word_info in alternative.words:
-                word = word_info.word
-                start_time = word_info.start_time.total_seconds()
-            
-
-                if word_info.word in keyword:
-                    word_time.append(start_time)
-                    
-
-
-        return word_time
-
-
-
-    word_time = transcribe_gcs("gs://short_test/audio.wav")
+def STT_detection(word_time, count = 35):
 
     score=[0 for i in range(count)]
     for i in word_time:
         frame_index = i/1
         score[frame_index] = 1 
-        
+    
+    print(score)
+
     return score
